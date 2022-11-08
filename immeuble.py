9cca7d7f587b59c9b0cf1d90b34e695a88e7337a
Natsu97122 <114024810+Natsu97122@users.noreply.github.com>
2022-11-08 16:18:50 -0400

    Add files via upload

--- immeuble.py
+++ immeuble.py
@@ -1,7 +1,6 @@
 from turtle import *
 from random import *
-
-speed(2)
+hideturtle()
 def immeuble(largeur):
     def facade():
         fillcolor("red")
@@ -12,16 +11,56 @@ def immeuble(largeur):
             forward(largeur)
             left(90)
         end_fill()
+        def fenetre():
+            up()
+            fillcolor("white")
+            forward(largeur/5)
+            left(90)
+            begin_fill()
+            forward(largeur/4)
+            right(90)
+            forward(largeur/6)
+            left(90)
+            forward(largeur/6)
+            left(90)
+            forward(largeur/6)
+            left(90)
+            forward(largeur/6)
+            end_fill()
+            forward(largeur/4)
+            left(270)
+            forward(largeur/5)
+            left(180)
+            down()
+            
+        fenetre()
     facade()
+    def porte():
+        forward(largeur/2)
+        fillcolor("brown")
+        begin_fill()
+        left(90)
+        forward(largeur/2)
+        right(90)
+        forward(largeur/4)
+        right(90)
+        forward(largeur/2)
+        right(90)
+        forward(largeur/4)
+        end_fill()
+        forward(largeur/2)  
+        right(180) 
+    porte()
     def nb_etages():
-            for nb_etages in range(3) :
+            for nb_etages in range(2) :
                 left(90)
                 forward(largeur)
                 right(90)
                 facade()
     nb_etages()
 
-
-
+    return immeuble
 
 immeuble(100)
+
+exitonclick()
