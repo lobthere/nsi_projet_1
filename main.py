cc7c13410210fe88f7167a7cab8b4c5e0736d727
lobthere <lobthere@protonmail.com>
2022-11-11 19:49:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
 from turtle import *
-import debut
-import toit
-import carre
-
-debut.debut()
-taille = 50
-
-carre.carre(taille, "red")
-
-left(90)
-penup()
-fd(taille)
-right(90)
-pendown()
-
-toit.toit(taille, "blue")
