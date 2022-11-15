85f80bf76b13753ed0f9bc3a29988275b6269426
lobthere <lolool.le.vrai@gmail.com>
2022-11-15 17:02:19 -0400

    Add files via upload

--- route.py
+++ route.py
@@ -1,39 +1,34 @@
-import turtle
+import turtle as t
 import random
 
-def  route():
-    hauteur_route = random.randint(100, 500)
-    turtle.up()
-    turtle.goto(-x/2, -y/2)
-    turtle.down()
-    turtle.color("black")
-    turtle.fillcolor("black")
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.forward(1080*2)
-        turtle.left(90)
-        turtle.forward(hauteur_route)
-        turtle.left(90)
-    turtle.end_fill()
-    turtle.up()
-    turtle.goto(0 ,0)
-    turtle.color("white")
-    turtle.down()
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.forward(100)
-        turtle.left(90)
-        turtle.forward(-100)
-        turtle.left(90)
-    turtle.end_fill()
+SCREENSIZEX = 600
+SCREENSIZEY = 900
+SCREENMAXRIGHT = (SCREENSIZEY / 2) * -1
+SCREENMAXDOWN = (SCREENSIZEX / 2) * -1
+SCREENMAXLEFT = SCREENSIZEY / 2
+SCREENMAXUP = SCREENSIZEX / 2
+HAUTEUROUTE = -100
 
+screen = t.Screen()
+screen.setup(SCREENSIZEY, SCREENSIZEX)
+t.up()
+t.goto(0,HAUTEUROUTE)
+t.down()
+t.goto(SCREENMAXRIGHT, HAUTEUROUTE)
+t.fillcolor("black")
+t.begin_fill()
+t.goto(SCREENMAXRIGHT, SCREENMAXDOWN)
+t.goto(SCREENMAXLEFT, SCREENMAXDOWN)
+t.goto(SCREENMAXLEFT, HAUTEUROUTE)
+t.goto(SCREENMAXRIGHT, HAUTEUROUTE)
+t.end_fill()
 
-x = 1920
-y = 1080
-
-if __name__ == "__main__":
-    turtle.setup(x, y)
-    turtle.width(1)
-
-
-route()
+t.penup()
+t.right(90)
+t.forward(((SCREENMAXDOWN + HAUTEUROUTE) / 2 + 30) * -1)
+t.left(90)
+t.forward(200)
+t.pendown()
+t.color("red")
+t.forward(20)
+t.exitonclick()
